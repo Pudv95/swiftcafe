8d05d5a2f32c1127005d38d402cb933b72ca8293
Pudv95 <vivekupadhayay18@gmail.com>
2024-04-07 23:28:16 +0530

    add theme for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:swiftcafe/utils/constants/theme.dart';
 
 void main(){
   return runApp(MaterialApp.router(
-
+      theme: themeData,
   ));
 }
--- lib/utils/constants/theme.dart
+++ lib/utils/constants/theme.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+ThemeData themeData = ThemeData(
+  primarySwatch: Colors.blue,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      color: Colors.white,
+      fontSize: 16.0,
+    ),
+    displayLarge: TextStyle(
+      color: Colors.white,
+      fontSize: 24.0,
+    ),
+    displayMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+    ),
+    displaySmall: TextStyle(
+      color: Colors.white,
+      fontSize: 18.0,
+    ),
+  ),
+);
