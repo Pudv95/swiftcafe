29df676362463a6ca47df38c16663d277f513a23
Pudv95 <vivekupadhayay18@gmail.com>
2024-04-07 23:31:57 +0530

    add basic route design

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:swiftcafe/utils/constants/theme.dart';
+import 'package:swiftcafe/utils/routers/routes.dart';
 
 void main(){
   return runApp(MaterialApp.router(
-      theme: themeData,
+    theme: themeData,
+    routerDelegate: Routes().router.routerDelegate,
+    routeInformationParser: Routes().router.routeInformationParser,
   ));
 }
--- lib/utils/routers/routes.dart
+++ lib/utils/routers/routes.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+class Routes{
+  GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        name: 'login',
+        path: '/',
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return const MaterialPage(child: Placeholder());
+        },
+      ),
+      GoRoute(
+        name: 'dashboard',
+        path: '/dashboard',
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return const MaterialPage(child: Placeholder());
+        },
+      ),
+    ],
+  );
+}
